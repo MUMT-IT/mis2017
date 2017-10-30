fdabc302e6b4ccbe39331e757e3f13cbc46391dc
Likit Preeyanon <likit.pre@mahidol.edu>
2017-10-30 18:54:47 +0700

    added google authentication and authorization

--- webapp/app/auth/views/__init__.py
+++ webapp/app/auth/views/__init__.py
@@ -1,6 +1,77 @@
-from flask import render_template
+import google.oauth2.credentials
+import googleapiclient.discovery
+import google_auth_oauthlib.flow
+from flask import render_template, url_for, redirect, session, jsonify, request
 from .. import auth
+
+CLIENT_SECRETS_FILE = 'client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+API_SERVICE_NAME = 'drive'
+API_VERSION = 'v2'
 
 @auth.route('/')
 def main():
-    return render_template("auth/main.html")
+    if 'credentials' not in session:
+        return render_template("auth/main.html")
+    else:
+        credentials = google.oauth2.credentials.Credentials(
+            **session['credentials']
+        )
+        drive = googleapiclient.discovery.build(
+            API_SERVICE_NAME, API_VERSION, credentials=credentials
+        )
+        files = drive.files().list().execute()
+        session['credentials'] = credentials_to_dict(credentials)
+        return jsonify(**files)
+
+
+@auth.route('/login')
+def google_login():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow.redirect_uri = url_for('auth.oauth2callback', _external=True)
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true'
+    )
+    session['state'] = state
+    return redirect(authorization_url)
+
+
+@auth.route('/oauth2callback')
+def oauth2callback():
+    state = session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state
+    )
+    flow.redirect_uri = url_for('auth.oauth2callback', _external=True)
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+    # save these credentials in a persistent database instead
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+    return redirect(url_for('auth.main'))
+
+
+@auth.route('/revoke')
+def revoke():
+    if 'credentials' not in session:
+        return ('You need to authorize before testing the credentials.')
+    credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+    revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
+        params={'token': credentials.token},
+        headers={'content-type': 'application/x-www-form-urlencoded'})
+    status_code = getattr(revoke, 'status_code')
+    if status_code == 200:
+        return ('Credentials successfully revoked.')
+    else:
+        return ('An error occured.')
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes
+            }
--- webapp/app/main.py
+++ webapp/app/main.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, render_template
-from auth import auth as auth_blueprint
-
 app = Flask(__name__)
+app.secret_key = 'flasky'
 
+from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 
 @app.route('/')
@@ -11,4 +12,5 @@ def main():
 
 
 if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.run(debug=True)
