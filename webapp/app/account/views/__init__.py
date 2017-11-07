a287d02a685e613e45e8109e3e4ee530beb839a3
Likit Preeyanon <likit.pre@mahidol.edu>
2017-11-07 07:55:51 +0700

    added feature that allows user to add files to a cart for sending

--- webapp/app/account/views/__init__.py
+++ webapp/app/account/views/__init__.py
@@ -1,43 +1,49 @@
-from flask import render_template
+from flask import render_template, jsonify, session, redirect, url_for, request
 from flask_login import current_user, login_required
 from .. import account
 import google.oauth2.credentials
 import googleapiclient.discovery
 import pprint
 
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-API_SERVICE_NAME = 'drive'
-API_VERSION = 'v2'
-
-
-def get_service():
+def get_group_service():
     credentials = google.oauth2.credentials.Credentials(
         **current_user.login.credentials
     )
-    drive = googleapiclient.discovery.build(
-        API_SERVICE_NAME, API_VERSION, credentials=credentials
+    group_service = googleapiclient.discovery.build(
+        'admin', 'directory_v1', credentials=credentials
     )
-    return drive
+    return group_service
+
+
+def get_drive_service():
+    credentials = google.oauth2.credentials.Credentials(
+        **current_user.login.credentials
+    )
+    drive_service = googleapiclient.discovery.build(
+        'drive', 'v2', credentials=credentials
+    )
+    return drive_service
 
 
 def search_folder(title):
-    drive = get_service()
-    response = drive.files().list(q="title='MISDocs' and trashed = false", spaces='drive').execute()
+    drive_service = get_drive_service()
+    response = drive_service.files().list(q="title='MISDocs' and trashed = false", spaces='drive').execute()
     folders = response.get('items', [])
-    pprint.pprint(folders)
+    #pprint.pprint(folders)
     return folders
 
 
 def create_system_folder():
-    drive = get_service()
+    drive_service = get_drive_service()
     folder_metadata = {
         'title': 'MISDocs',
         'mimeType': 'application/vnd.google-apps.folder'
     }
-    folder = drive.files().insert(body=folder_metadata, fields='id').execute()
+    folder = drive_service.files().insert(body=folder_metadata, fields='id').execute()
     return folder
 
 
+@login_required
 @account.route('/profile')
 def view_profile():
     return render_template('account/profile.html')
@@ -46,7 +52,7 @@ def view_profile():
 @login_required
 @account.route('/docs')
 def view_docs():
-    drive = get_service()
+    drive_service = get_drive_service()
     folders = []
     folders = search_folder('MISDocs')  # search for system folder
     if not folders:
@@ -56,20 +62,68 @@ def view_docs():
         print('MISDocs found.')
         folder = folders[0]
 
-    children = drive.children().list(folderId=folder['id']).execute()['items']
+    children = drive_service.children().list(folderId=folder['id']).execute()['items']
     files = []
     for child in children:
-        f = drive.files().get(fileId=child['id']).execute()
-        pprint.pprint(f)
+        f = drive_service.files().get(fileId=child['id']).execute()
         files.append(f)
     return render_template('account/docs.html', folder=folder, files=files)
 
+
+@login_required
 @account.route('/docs/delete')
 def delete():
-    drive = get_service()
+    drive_service = get_drive_service()
     folders = search_folder('MISDocs')
     for f in folders:
-        drive.files().delete(fileId=f['id']).execute()
+        drive_service.files().delete(fileId=f['id']).execute()
         print('deleted %s' % f['id'])
     return 'Done'
 
+
+@login_required
+@account.route('/docs/folder/<folder_title>/<folder_id>')
+def open_folder(folder_title, folder_id):
+    if not folder_id:
+        return redirect(url_for('account.view_docs'))
+    else:
+        files = get_children_files(folder_id)
+        return render_template('account/file_list.html', folder_title=folder_title, files=files)
+
+
+def get_children_files(folder_id):
+    drive_service = get_drive_service()
+    children = drive_service.children().list(folderId=folder_id).execute()['items']
+    files = []
+    for child in children:
+        f = drive_service.files().get(fileId=child['id']).execute()
+        files.append(f)
+    return files
+
+
+@account.route('/groups')
+def manage_groups():
+    group_service = get_group_service()
+    group_email = current_user.email
+    results = group_service.users().list().execute()
+    return jsonify(results)
+
+
+@login_required
+@account.route('/cart')
+def view_cart():
+    file_id = request.args.get('file_id', None)
+    if file_id:
+        if 'files_cart' not in session:
+            session['files_cart'] = [file_id]
+        else:
+            _files = session['files_cart']
+            if file_id not in _files:
+                _files.append(file_id)
+                session['files_cart'] = _files
+    drive_service = get_drive_service()
+    files = []
+    for item in session['files_cart']:
+        f = drive_service.files().get(fileId=item).execute()
+        files.append(f)
+    return render_template('account/cart.html', files=files)
